ead6ef983978864d876a571547749e87c2480175
Dimas Purwanto <dimasmaspur@gmail.com>
2021-07-14 08:41:32 +0700

    jancuk

--- tambah_penjualan.php
+++ tambah_penjualan.php
@@ -5,14 +5,16 @@ include 'koneksi.php';
 $inputJSON = file_get_contents('php://input');
 $input = json_decode($inputJSON, TRUE);
 
-if (isset($input['id'])) {
+if (isset($input['id_user'])) {
+    $tanggal = date('Y-m-d');
+    $id_user = $input['id_user'];
+    $id_cupang = empty($input['id_cupang']) ? "" : $input['id_cupang'];
+    $nama_pembeli =  empty($input['nama_pembeli']) ? "" : $input['nama_pembeli'];
+    $notelp_pembeli = empty($input['notelp_pembeli']) ? "" : $input['notelp_pembeli'];
+    $alamat_pembeli = empty($input['alamat_pembeli']) ? "" : $input['alamat_pembeli'];
+    $harga_penjualan = empty($input['harga_penjualan']) ? "" : $input['harga_penjualan'];
 
-    $id_gejala = $input['id'];
-    $status = empty($input['status1']) ? "" : $input['status1'];
-
-    $q = "UPDATE datapengiriman SET
-			status1='$status'
-		WHERE id='$id_gejala'";
+    $q = "INSERT INTO penjualan(id_user,id_cupang,nama_pembeli,notelp_pembeli, alamat_pembeli, harga_penjualan, approve, date) VALUES ('$id_user','$id_cupang','$nama_pembeli','$notelp_pembeli','$alamat_pembeli','$harga_penjualan','false','$tanggal')";
     mysqli_query($con, $q);
 
     $response["status"] = 0;
